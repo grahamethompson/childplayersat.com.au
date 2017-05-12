e87044b2967d4141daec7ade9c534b3733cadb7d
Grahame T <gthompson@questacon.edu.au>
2017-05-12 13:25:26 +1000

    init

---
+++ templates/yoo_tweety/lib/js/template.js.php.js
@@ -0,0 +1,111 @@
+/* Copyright (C) 2007 - 2009 YOOtheme GmbH */
+
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('3 1a={19:6(15,s,r){3 9=0;3 n=[];3 q=15.18(" ");3 I=q.1b();3 u=\'\';q.g(6(8,i){u+=\'.t("\'+8+\'")\'});$1c(I).g(6(m,i){1e(\'n.17(m\'+u+\');\')});n.g(6(4,i){7(!$o(4))13;3 a,j;7(4.E){a=4.E;j=0;j+=4.K(\'P-1d\').N();j+=4.K(\'P-1f\').N();a-=j;7(s!=W){a-=s}}A 7(4.G.H){a=4.G.H}9=X.F(9,a)});7(r!=W){9=X.F(9,r)}n.g(6(4,i){7(!$o(4))13;7(1u.1t){4.J(\'14\',9+\'L\')}A{4.J(\'1g-14\',9+\'L\')}})}};3 U=c T({V:6(m,11,Z,x,10,k){b.l({y:1y,Y:v.O.1z,Q:M,p:\'\'},x);3 5=b.5;$$(m).g(6(8,i){3 w=8;7(k&&8.t(k)){w=8.t(k)}3 f=c v.1k(w,5);7(!($o(5.p)&&8.1h(5.p))){8.12(\'1m\',6(e){f.l(5,x).C(11)});8.12(\'1p\',6(e){f.l(5,10).C(Z)})}})}});U.R(c S);3 D=c T({V:6(5){b.l({Y:v.O.1l,y:1s,Q:M,h:[\'#1x\',\'#1n\']},5);3 B=c 1o(1q.B);3 f=B.1i(b.5);3 d=0;3 h=b.5.h;3 1j=z.1r(b.5.y*2);z();6 z(){f.C({\'1w-1v\':h[d]});7(d+1>=h.16){d=0}A{d++}}}});D.R(c S);',62,98,'|||var|div|options|function|if|el|maxHeight|divHeight|this|new|index||fx|each|colors||divPadding|elementFx|setOptions|element|matchDivs|chk|ignoreClass|selectors|minWidth|divBorder|getElement|script|Fx|elfx|enterFx|duration|animate|else|body|start|YOOBackgroundFx|offsetHeight|max|style|pixelHeight|elements|setStyle|getStyle|px|false|toInt|Transitions|padding|wait|implement|Options|Class|YOOMorph|initialize|undefined|Math|transition|leave|leaveFx|enter|addEvent|return|height|selector|length|push|split|matchDivHeight|YOOBase|shift|ES|top|eval|bottom|min|hasClass|effects|timer|Styles|linear|mouseenter|999999|Element|mouseleave|document|periodical|9000|ie6|window|color|background|FFFFFF|500|expoOut'.split('|'),0,{}))
+/* Copyright (C) 2007 - 2009 YOOtheme GmbH */
+
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('5 t=6 v({w:4(q,p,2){0.x({r:\'m\'},2);0.7=q;0.9=p;y(0.2.r){A\'z\':0.j();C;m:0.g()}},g:4(){5 2={};e(!$n(0.2.8)&&!$n(0.2.c)){2={c:-1}}$k(0.7).l(4(3,i){e(3.o(\'s\'))2={c:i}}.u(0));5 B=6 d.Q(0.7,0.9,$N(0.2,2))},j:4(){$k(0.7).l(4(3,i){5 a=3.h(\'a\');5 f=3.h(0.9);5 b=6 d.M(f,{D:d.P.R,L:K});e(!(3.o(\'s\')||0.2.8==\'F\'||0.2.8==i)){b.E()}a.G(\'H\',4(){b.J()})}.u(0))}});t.I(6 O);',54,54,'this||options|tog|function|var|new|togs|display|elms|span|fx|show|Fx|if|ul|createDefault|getElement||createSlide|ES|each|default|defined|hasClass|elements|togglers|accordion|active|YOOAccordionMenu|bind|Class|initialize|setOptions|switch|slide|case|accordionMenu|break|transition|hide|all|addEvent|click|implement|toggle|250|duration|Slide|extend|Options|Transitions|Accordion|linear'.split('|'),0,{}))
+/* Copyright (C) 2007 - 2009 YOOtheme GmbH */
+
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('h 16=d R({1g:8(e,7){4.N({1e:1l.1s.1p,I:1n,1o:F,10:R.1q,f:1,g:\'n\',18:1t,W:[\'1j\',\'1f\',\'1d\',\'1k\',\'1i\',\'1h\',\'1m\',\'1v\'],U:\'6.1B\',T:\'6.V\'},7);4.e=$(e),4.c=4.e.Q(4.7.T);4.6=[];4.a=[];4.e.1C(4.7.U).Z(8(5,i){4.X(5,i);5.w(\'1E\',8(E){4.1b(E,5)}.k(4));5.w(\'1u\',8(){4.1c(5,i)}.k(4));b(4.7.g==\'n\'){5.w(\'S\',8(){4.G(4.c,i)}.k(4))}B{5.w(\'S\',8(){4.G(5,i)}.k(4))}}.k(4));b(4.7.g==\'n\'){b(4.c){4.r(4.c)}B{h s=4.e.Q(\'6\');s.Y(\'V\');s.Y(\'c\');4.r(s)}}},X:8(5,i){b(4.7.g==\'n\'&&i!=0)14;h K=\'u \'+\'P\'+(i+1);4.7.W.Z(8(H,i){b(5.1w(H)){K+=\' P-\'+H}});4.a[i]=d o(\'a\',{\'A\':\'u-1\'}).M(d o(\'a\',{\'A\':\'u-2\'}).M(d o(\'a\',{\'A\':\'u-3\'})));4.a[i].9=4.a[i].17(4.7);4.6[i]=d o(\'6\',{\'A\':K}).M(4.a[i]).1D(4.e);4.6[i].9=4.6[i].17(4.7)},r:8(5){4.6[0].1A({\'y\':5.j,\'z\':5.m,\'1z\':\'1F\',\'f\':4.7.f});4.c=5},1b:8(E,5){b(!4.c)4.r(5);4.c=5;4.7.10(d 1x(E),5)},1c:8(5,i){13(4.7.g){q\'12\':4.O(5,i,19);v;q\'1a\':4.J(5,i,19);v;11:4.L(5,0)}},G:8(5,i){13(4.7.g){q\'12\':4.O(5,i,F);v;q\'1a\':4.J(5,i,F);v;11:4.L(5,0)}},L:8(5,i){b(!4.c)14;4.6[i].9.l({\'y\':[4.6[i].j,5.j],\'z\':[4.6[i].m,5.m]})},O:8(5,i,D){b(D){4.6[i].9.N(4.7);4.6[i].9.C({\'y\':5.j,\'z\':5.m});4.6[i].9.l({\'f\':[0,1]})}B{h 15=4.7.I*2;4.6[i].9.N({I:15});4.6[i].9.l({\'f\':[1,0]})}},J:8(5,i,D){h x=4.7.18;b(D){4.6[i].9.C({\'f\':1,\'y\':5.j,\'z\':5.m});4.a[i].9.C({\'t-p\':x});4.a[i].9.l({\'t-p\':[x,0]})}B{4.a[i].9.C({\'t-p\':0});4.a[i].9.l({\'t-p\':[0,x]})}}});16.1r(d 1y);',62,104,'||||this|item|li|options|function|fx|div|if|current|new|menu|opacity|mode|var||offsetLeft|bind|custom|offsetWidth|move|Element|top|case|setCurrent|first|margin|fancy|break|addEvent|offset|left|width|class|else|set|show|event|false|mouseleaveItem|col|duration|slideFx|css|moveFx|adopt|setOptions|fadeFx|bg|getElement|Class|mouseleave|activeSelector|itemSelector|active|colorSelector|createBackground|addClass|each|onClick|default|fade|switch|return|dur|YOOFancyMenu|effects|slideOffset|true|slide|clickItem|mouseenterItem|blue|transition|pink|initialize|yellow|orange|red|green|Fx|lilac|500|wait|sineInOut|empty|implement|Transitions|30|mouseenter|turquoise|hasClass|Event|Options|visibility|setStyles|level1|getElements|injectInside|click|visible'.split('|'),0,{}))
+/* Copyright (C) 2007 - 2009 YOOtheme GmbH */
+
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('2 g=f q({u:6(e,7){9.x({i:\'w\',y:s,B:h.v.A,z:r},7);2 3={\'8\':0,\'a\':0,\'b\':0};t(9.7.i){j\'8\':3={\'8\':0,\'b\':0};m;j\'a\':3={\'a\':0,\'b\':0};m}$$(e).M(6(4){2 1=4.L(\'1\');k(1){2 5=f h.N(1,9.7);2 n=1.O(\'8\',\'a\',\'b\');1.l(3);4.Q({C:6(){2 c=4.K();k(c.J(\'d\')==\'p\')c.o(\'d\',\'E\');5.e.o(\'d\',\'p\');5.D(n)},F:6(){5.G();1.l(3)}})}}.I(9))}});g.H(f P);',53,53,'|ul|var|reset|li|fx|function|options|width|this|height|opacity|parent|overflow|element|new|YOODropdownMenu|Fx|mode|case|if|setStyles|break|styles|setStyle|hidden|Class|false|600|switch|initialize|Transitions|default|setOptions|duration|wait|linear|transition|mouseenter|start|visible|mouseleave|stop|implement|bind|getStyle|getParent|getElement|each|Styles|getStyles|Options|addEvents'.split('|'),0,{}))
+/* Copyright (C) 2007 - 2009 YOOtheme GmbH */
+
+var YOOTools = {
+		
+	start: function() {
+		
+		/* Match height of div tags */
+		YOOTools.setDivHeight();
+
+		/* Accordion menu */
+		new YOOAccordionMenu('div#middle ul.menu li.toggler', 'ul.accordion', { accordion: 'slide' });
+
+		/* Dropdown menu */
+		new YOODropdownMenu('div#menu li.parent', { mode: 'height', transition: Fx.Transitions.Expo.easeOut });
+
+		/* Morph: main menu - level1 (tab) */
+		var menuEnter = { 'color': '#000000', 'background-color': '#FFFFFF', 'margin-top': 0, 'padding-bottom': 5, 'background-position': '0 35' };
+		var menuLeave = { 'color': '#787878', 'background-color': '#EBEBEB', 'margin-top': 5, 'padding-bottom': 0, 'background-position': '0 24' };
+
+		if(window.ie) { $$('div#menu li.level1 a.level1').each(function(elm){ elm.setStyle('background-position', '0 24'); }); }
+		if(window.ie6) { $$('div#menu li.level1 a.level1').each(function(elm){ elm.setStyles({ 'background-color': '#EBEBEB', 'background-position': '0 25' }); }); }
+		if(window.ie6) { $$('div#menu li.active a.level1').each(function(elm){ elm.setStyle('background-color', '#FFFFFF'); }); }
+
+		new YOOMorph('div#menu li.level1', menuEnter, menuLeave,
+			{ transition: Fx.Transitions.linear, duration: 100, ignoreClass: 'active'},
+			{ transition: Fx.Transitions.sineIn, duration: 300 }, 'a.level1');
+
+		/* Morph: main menu - level1 subline (color) */
+		menuEnter = { 'color': '#000000' };
+		menuLeave = { 'color': '#505050' };
+		
+		new YOOMorph('div#menu li.level1', menuEnter, menuLeave,
+			{ transition: Fx.Transitions.linear, duration: 100, ignoreClass: 'active'},
+			{ transition: Fx.Transitions.sineIn, duration: 300 }, 'span.sub');
+
+		/* Morph: main menu - level2 and deeper (color) */
+		menuEnter = { 'color': '#000000', 'background-color': '#EBEBEB' };
+		menuLeave = { 'color': '#646464', 'background-color': '#FFFFFF' };
+		
+		var selector = 'div#menu li.level2 a, div#menu li.level2 span.separator';
+		/* fix for Opera because Mootools 1.1 is not compatible with latest Opera version */
+		if (window.opera) { selector = 'div#menu li.item1 li.level2 a, div#menu li.item1 li.level2 span.separator, div#menu li.item2 li.level2 a, div#menu li.item2 li.level2 span.separator, div#menu li.item3 li.level2 a, div#menu li.item3 li.level2 span.separator, div#menu li.item4 li.level2 a, div#menu li.item4 li.level2 span.separator, div#menu li.item5 li.level2 a, div#menu li.item5 li.level2 span.separator, div#menu li.item6 li.level2 a, div#menu li.item6 li.level2 span.separator, div#menu li.item7 li.level2 a, div#menu li.item7 li.level2 span.separator'; }
+		
+		new YOOMorph(selector, menuEnter, menuLeave,
+			{ transition: Fx.Transitions.linear, duration: 0 },
+			{ transition: Fx.Transitions.sineIn, duration: 500 });
+
+		/* Morph: sub menu - level1 */
+		var submenuEnter = { 'background-color': '#FFFFFF'};
+		var submenuLeave = { 'background-color': '#ebebeb'};
+
+		new YOOMorph('div#middle ul.menu a.level1, div#middle ul.menu span.level1', submenuEnter, submenuLeave,
+			{ transition: Fx.Transitions.expoOut, duration: 0 },
+			{ transition: Fx.Transitions.sineIn, duration: 300 });
+
+		/* Morph: sub menu - level2 and deeper */
+		var submenuEnter = { 'background-color': '#FFFFFF'};
+		var submenuLeave = { 'background-color': '#f5f5f5'};
+
+		new YOOMorph('div#middle ul.menu li.level2 a, div#middle ul.menu li.level2 span.separator', submenuEnter, submenuLeave,
+			{ transition: Fx.Transitions.expoOut, duration: 0 },
+			{ transition: Fx.Transitions.sineIn, duration: 300 });
+
+		/* Smoothscroll */
+		new SmoothScroll({ duration: 500, transition: Fx.Transitions.Expo.easeOut });
+	},
+
+	/* Include script */
+	include: function(library) {
+		$ES('script').each(function(s, i){
+			var src  = s.getProperty('src');
+			var path = '';
+			if (src && src.match(/yoo_tools\.js(\?.*)?$/)) path = src.replace(/yoo_tools\.js(\?.*)?$/,'');
+			if (src && src.match(/template\.js\.php(\?.*)?$/)) path = src.replace(/template\.js\.php(\?.*)?$/,'');
+			if (path != '') document.write('<script language="javascript" src="' + path + library + '" type="text/javascript"></script>');
+		});
+	},
+
+	/* Match height of div tags */
+	setDivHeight: function() {
+		YOOBase.matchDivHeight('div.topbox div.deepest', 0, 40);
+		YOOBase.matchDivHeight('div.bottombox div.deepest', 0, 40);
+		YOOBase.matchDivHeight('div.maintopbox div.deepest', 0, 40);
+		YOOBase.matchDivHeight('div.mainbottombox div.deepest', 0, 40);
+		YOOBase.matchDivHeight('div.contenttopbox div.deepest', 0, 40);
+		YOOBase.matchDivHeight('div.contentbottombox div.deepest', 0, 40);
+	}
+
+};
+
+/* Add functions on window load */
+window.addEvent('domready', YOOTools.start);
+
+/* Load IE6 fix */
+if (window.ie6) {
+	YOOTools.include('addons/ie6fix.js');
+	YOOTools.include('addons/ie6png.js');
+	YOOTools.include('yoo_ie6fix.js');
+}
